5f56977ef78f2c7d20938d269be037c029b3414a
Ian Vassallo <spideramerica71@gmail.com>
2024-01-06 00:47:00 -0600

    feat: config common module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+
+import { DatabaseModule } from '@app/common';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
