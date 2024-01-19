e173f9f6b52ebe4153ae8e8babf175b391446772
Ian Vassallo <spideramerica71@gmail.com>
2024-01-19 13:45:08 -0600

    feat: Simple Validation

--- apps/reservations/src/dto/create-reservation.dto.ts
+++ apps/reservations/src/dto/create-reservation.dto.ts
@@ -1,6 +1,20 @@
+import { Type } from 'class-transformer';
+import { IsDate, IsNotEmpty, IsString } from 'class-validator';
+
 export class CreateReservationDto {
+  @Type(() => Date)
+  @IsDate()
   startDate: Date;
+
+  @Type(() => Date)
+  @IsDate()
   endDate: Date;
+
+  @IsString()
+  @IsNotEmpty()
   placeId: string;
+
+  @IsString()
+  @IsNotEmpty()
   invoiceId: string;
 }
